225f00d51d7c1f0eb8dc28dc2b5c2acb0cfd609d
brianmoney <61640534+brianmoney@users.noreply.github.com>
2025-03-20 15:54:58 -0400

    more copilot update

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import path from 'path';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -34,6 +37,10 @@ app.use((req, res, next) => {
   });
 
   next();
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
 
 (async () => {
