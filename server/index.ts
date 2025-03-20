cb411ca08ec82dbfa0fea3e04623ad84f2661e6f
brianmoney <61640534+brianmoney@users.noreply.github.com>
2025-03-20 16:12:24 -0400

    gemini

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 app.use((req, res, next) => {
   const start = Date.now();
