31af4f0dc829ee1251d933d5747f483a4b3ba7cc
brianmoney <61640534+brianmoney@users.noreply.github.com>
2025-03-21 09:27:26 -0400

    another try to link to manuals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import ManualPage from './pages/ManualPage';
+import FS0101 from '../../pages/FS0101';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/manuals/:manualId" element={<ManualPage />} />
+          <Route path="/pages/fs0101_manual" element={<FS0101 />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Toaster />
