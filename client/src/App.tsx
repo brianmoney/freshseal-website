f21c202ed9688e83e1811e354b8b656c4344f7dd
brianmoney <61640534+brianmoney@users.noreply.github.com>
2025-03-21 14:00:39 -0400

    path update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route path="/pages/FS0101" element={<FS0101 />} />
+          <Route path="/pages/fs0101-manual" element={<FS0101 />} />
           <Route path="/manuals/:manualId" element={<ManualPage />} />
           <Route path="/pages/:manualId" element={<ManualPage />} />
           <Route path="/" element={<Home />} />
